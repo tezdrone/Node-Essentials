c797a71c906136ec495102f48cc214048fefaf38
Ben <mineeraft@gmail.com>
2021-04-10 12:57:13 +1200

    Add interval

--- timers.js
+++ timers.js
@@ -1,7 +1,18 @@
 const waitTime = 3000;
+const waitInterval = 500;
+let currentTime = 0;
+
+const incTime = () => {
+	currentTime += waitInterval;
+	console.log(`waiting ${currentTime / 1000} seconds`);
+};
 
 console.log(`setting a ${waitTime / 1000} second delay`);
 
-const timerFinished = () => console.log('done');
+const timerFinished = () => {
+	clearInterval(interval);
+	console.log('done');
+};
 
+const interval = setInterval(incTime, waitInterval);
 setTimeout(timerFinished, waitTime);
